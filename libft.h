3f668245dcb4e760d57289013d0863320f1937c4
Lauri Salmi <lasalmi@c2r6p11.hive.fi>
2021-11-03 15:07:55 +0200

    First push

---
+++ libft.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/03 14:56:51 by lasalmi           #+#    #+#             */
+/*   Updated: 2021/11/03 15:06:52 by lasalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+size_t	ft_strlen(const char *s);
+void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
+#endif
