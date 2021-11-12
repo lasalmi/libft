84c0984469a60e691ec16566901c6420196e70b8
Lauri Salmi <lasalmi@c1r2p5.hive.fi>
2021-11-12 11:32:30 +0200

    Pushing modified functions

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,12 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 08:22:58 by lasalmi           #+#    #+#             */
-/*   Updated: 2021/11/11 15:00:55 by lasalmi          ###   ########.fr       */
+/*   Updated: 2021/11/12 11:32:08 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "libft.h"
-#include <stdio.h>
 
 size_t  ft_strlcat(char *dest, const char *src, size_t size)
 {
@@ -22,13 +21,12 @@ size_t  ft_strlcat(char *dest, const char *src, size_t size)
 	i = 0;
 	j = 0;
 
-	while (dest[i] != '\0' && size > i)
+	while (dest[i] != '\0' && size > i + 1)
 		i++;
 	if (dest[i] != '\0' && size == i + 1)
 		return (size + ft_strlen(src));
 	while (size > (i + 1) && src[j] != '\0')
 		dest[i++] = src[j++];
 	dest[i] = '\0';
-	printf("SRC: %i DEST: %i", (int)ft_strlen(src), (int)ft_strlen(dest));
 	return (i);
 }
