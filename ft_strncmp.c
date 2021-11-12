2d8ac609abdc7b1f8440b8d801b8e87d08f169d1
Lauri Salmi <lasalmi@c1r2p5.hive.fi>
2021-11-12 11:54:46 +0200

    Pushing strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 11:50:47 by lasalmi           #+#    #+#             */
+/*   Updated: 2021/11/12 11:54:30 by lasalmi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	*str2;
+
+	str = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (str[i] == str2[i] && str[i] != '\0' && str2[i] != '\0' && (i+1) < n)
+		i++;
+	return (str[i] - str2[i]);
+}
