a4e997d95b8eed69ba074bf691cba2bac4fa4d96
Lauri Salmi <lasalmi@c1r5p21.hive.fi>
2021-11-18 13:56:04 +0200

    LAAAA

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 12:13:20 by lasalmi           #+#    #+#             */
-/*   Updated: 2021/11/18 13:54:52 by lasalmi          ###   ########.fr       */
+/*   Updated: 2021/11/18 13:55:51 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@ int	ft_isalpha(int	c)
 	unsigned char	char_to_compare;
 
 	char_to_compare = (unsigned char)c;
-	if  (c < 0 || c > 255);
+	if  (c < 0 || c > 255)
+		return (0);
 	if (char_to_compare >= (unsigned char)'A' && char_to_compare <= (unsigned char)'Z')
 		return (1);
 	if (char_to_compare >= (unsigned char)'a' && char_to_compare <= (unsigned char)'z')
