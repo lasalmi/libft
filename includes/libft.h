ce51f4c9d1714c4816ff24fa4b14236217e072fc
Lauri Salmi <lasalmi@student.hive.fi>
2021-11-25 15:19:42 +0200

    Pushing lstiter

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:56:51 by lasalmi           #+#    #+#             */
-/*   Updated: 2021/11/25 15:09:11 by lasalmi          ###   ########.fr       */
+/*   Updated: 2021/11/25 15:15:06 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,4 +76,5 @@ t_list	*ft_lstnew(void const *content, size_t content_size);
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void	ft_lstadd(t_list **alst, t_list *new);
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 #endif
