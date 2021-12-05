4ed5bdc32fb9bb3a00c79dfd2c9c751aa8889bf6
Lauri Salmi <lasalmi@Lauris-MacBook-Pro.local>
2021-12-05 14:23:23 +0200

    Added malloc prot to lstnew content

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 14:11:34 by lasalmi           #+#    #+#             */
-/*   Updated: 2021/12/02 14:20:08 by lasalmi          ###   ########.fr       */
+/*   Updated: 2021/12/05 14:21:53 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 	else
 	{
 		newlink->content = ft_memalloc(content_size);
+		if (!newlink->content)
+			return (NULL);
 		ft_memcpy(newlink->content, content, content_size);
 		newlink->content_size = content_size;
 		newlink->next = NULL;
