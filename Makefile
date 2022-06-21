fe39ca950481a57c5f5e8e360cbd18d89f41238e
Lauri Salmi <late.salmi@gmail.com>
2022-06-21 11:10:53 +0300

    Adding ft_is_in_range.c

--- Makefile
+++ Makefile
@@ -103,6 +103,7 @@ $(SRCDIR)ft_atol.c \
 $(SRCDIR)ft_atoll.c \
 $(SRCDIR)ft_swap.c \
 $(SRCDIR)ft_quicksortint.c \
+$(SRCDIR)ft_is_in_range.c \
 $(SRCDIR)ft_abs.c
 OBJECTS := $(SRCS:.c=.o)
 SRCDIR=./
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 14:56:51 by lasalmi           #+#    #+#             */
-/*   Updated: 2022/06/15 13:59:29 by lasalmi          ###   ########.fr       */
+/*   Updated: 2022/06/21 11:10:15 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -262,6 +262,7 @@ int			ft_snprintf(char *str, size_t size, const char *input_format, ...);
 void		ft_pf_mallocfail(t_strdata *strdata);
 void		ft_quicksortint(int *arr, int len);
 int			ft_abs(int value);
+int			ft_is_in_range(int nb, int start, int end);
 static const t_myfunc	g_funcs[] = {
 	ft_read_format,
 	ft_pf_read_specifiers,
