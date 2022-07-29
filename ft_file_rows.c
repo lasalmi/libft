dd89e2a6f4dadfb13a55f5ce2f90a02bb1326c07
Lauri Salmi <late.salmi@gmail.com>
2022-07-29 10:38:38 +0300

    Adding open fail protection to file rows

--- ft_file_rows.c
+++ ft_file_rows.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_filerows.c                                      :+:      :+:    :+:   */
+/*   ft_file_rows.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lasalmi <lasalmi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 09:45:31 by lasalmi           #+#    #+#             */
-/*   Updated: 2022/07/28 17:57:12 by lasalmi          ###   ########.fr       */
+/*   Updated: 2022/07/29 10:38:13 by lasalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ size_t	ft_file_rows(char *file)
 
 	rows = 0;
 	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		return (0);
 	ret = get_next_line(fd, &line);
 	ft_strdel(&line);
 	if (ret > 0 && fd > 0)
